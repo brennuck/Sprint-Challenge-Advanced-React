edee4eb4fd5a5a26327ae0afe9f56f4c8c434c10
Brennon Nuckols <brennuck@gmail.com>
2020-02-05 11:20:44 -0700

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import PlayerList from './Components/PlayerList';
+import GoinDark from './Components/GoinDark';
+
+import "./styles.scss";
 
 function App() {
   return (
     <div className="App">
+      <GoinDark />
       <PlayerList />
     </div>
   )
